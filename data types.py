b2a7883e9c27066901e2babeb0cfb6450d16fa13
Knowledge to Wisdom <75301333+knowledgetowisdom@users.noreply.github.com>
2020-12-30 18:20:49 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,37 @@
+# Data Types
+'''
+Built-in Data Types
+
+Text Type: 	str
+Numeric Types: 	int, float, complex
+Sequence Types: 	list, tuple, range
+Mapping Type: 	dict
+Set Types: 	set, frozenset
+Boolean Type: 	bool
+Binary Types: 	bytes, bytearray, memoryview
+'''
+#integer
+x=5 #or x=int(5)
+print(x)
+
+#float 
+y= 4.5 #or y=float(4.5)
+print(y)
+
+#string
+z="wisdom" #or z=str("wisdom")
+print(z)
+
+#list
+my_list=["mango","banana","apple"] #or mylist=list(("mango","banana","apple"))
+print(my_list)
+
+#tuple
+my_tuple=("apple","mango","banana") #or my_tuple=tuple(("mango","banana","apple"))
+print(my_tuple)
+
+#dictionary 
+my_dic={"name":"john",
+        "age":25,
+        "ID":420}
+print(my_dic)
